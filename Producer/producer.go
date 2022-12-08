72a679d91c6cd016d2e747d938f3df67f3823a79
Pavel Karatkevich <p_korotkevitch@Pavels-MacBook-Air.local>
2022-12-09 00:26:01 +0100

    send first test events

---
+++ Producer/producer.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", "test_topic", 0)
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	_, err = conn.WriteMessages(
+		kafka.Message{Value: []byte("one!")},
+		kafka.Message{Value: []byte("two!")},
+		kafka.Message{Value: []byte("five!")},
+		kafka.Message{Value: []byte("My brand new message")},
+	)
+
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+}
